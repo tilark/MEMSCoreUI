90b91d8cb662bbc7fa1a2fddf243a14c82a07cd7
tilark <tilark.liu@gmail.com>
2018-05-23 22:55:55 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px"><Aside></Aside></el-aside>
+        <el-container>
+          <el-main>Main
+            <!-- <img src="./assets/logo.png"> -->
+            <router-view/>
+          </el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+import Aside from './components/Aside.vue'
+export default {
+  name: 'App',
+  components: {
+    Aside
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
