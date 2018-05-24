fe7209db4b4fcdeea3d6ee64a0c86666a3fa509f
tilark <tilark.liu@gmail.com>
2018-05-24 15:55:52 +0800

    添加echarts图表工具
    1. 添加echarts图表工具
    2. 在设备类别中添加一个测试数据饼图

--- src/components/DeviceStatistics.vue
+++ src/components/DeviceStatistics.vue
@@ -1,5 +1,75 @@
 <template>
   <section>
-    这是设备类别统计
+    <el-row>
+      <el-col :span="12">
+        <div id="charPie" style="width:100%; height:400px;"></div>
+      </el-col>
+    </el-row>
   </section>
 </template>
+<script>
+import echarts from 'echarts'
+export default {
+  data () {
+    return {
+      charPie: null
+    }
+  },
+  methods: {
+    drawPieCharts () {
+      var pieChart = echarts.init(document.getElementById('charPie'))
+      var option = {
+        title: {
+          text: 'Pie Chart',
+          subtext: '纯属虚构',
+          x: 'center'
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+          type: 'scroll',
+          orient: 'vertical',
+          left: 10,
+          top: 20,
+          bottom: 20,
+          data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+              {value: 335, name: '直接访问'},
+              {value: 310, name: '邮件营销'},
+              {value: 234, name: '联盟广告'},
+              {value: 135, name: '视频广告'},
+              {value: 1548, name: '搜索广告'}
+            ],
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      }
+      pieChart.setOption(option)
+    },
+    drawCharts () {
+      this.drawPieCharts()
+    }
+  },
+  mounted: function () {
+    this.drawCharts()
+  },
+  updated: function () {
+    this.drawCharts()
+  }
+}
+</script>
